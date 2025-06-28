d0bcbca31db2854ec271ac529b3cb845ee07aef6
mei <mail@meihapps.gay>
2025-06-28 19:03:29 +0100

    fix server-side current page indication

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,15 @@
 <header>
-    <?php
-    include "includes/pfp.php";
-    echo $_SERVER["REQUEST_URI"];
-    ?>
+    <?php include "includes/pfp.php"; ?>
     <h1>mei happs</h1>
     <p class="subtle">
         (she/it)
     </p>
     <nav>
         <a href="/" <?php if (
-            $_SERVER["PHP_SELF"] == "/index.php/"
+            $_SERVER["PHP_SELF"] == "/"
         ) { ?> class="active" <?php } ?>>home</a>
         <a href="/blog" <?php if (
-            $_SERVER["PHP_SELF"] == "/index.php/blog"
+            $_SERVER["PHP_SELF"] == "/blog"
         ) { ?> class="active" <?php } ?>>blog</a>
         <p></p>
     </nav>
