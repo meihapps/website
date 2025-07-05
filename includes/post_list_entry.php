251c57c3eed1f2f0e0fdff2fef7cfbea12445c59
mei <mail@meihapps.gay>
2025-07-05 19:34:46 +0100

    accessibility improvement with header levels

--- includes/post_list_entry.php
+++ includes/post_list_entry.php
@@ -2,11 +2,11 @@
 /** @var array{title: string, link: string, date: string|null} $post */
 ?>
 <article>
-    <h3>
+    <h2>
         <a href="<?= $post["link"] ?>">
             <?= $post["title"] ?>
         </a>
-    </h3>
+    </h2>
     <?php if ($post["date"]): ?>
         <time datetime="<?= $post["date"] ?>" class="subtle">
             <?= date("F j, Y", strtotime($post["date"])) ?>
